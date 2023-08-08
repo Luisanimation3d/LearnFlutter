7d1aad5177b5ccc8794164d15da5181449a1f087
Luisanimation3d <luisanimation3d@gmail.com>
2023-08-08 08:55:54 -0500

    Learn List View

--- flutter_application_3/lib/listView.dart
+++ flutter_application_3/lib/listView.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 List<String> list = ['Map', 'Operative System', 'File'];
 List<IconData> icons = [Icons.map, Icons.computer, Icons.folder];
+List<Color> colors = [Colors.blue, Colors.red, Colors.green];
 
 class listView extends StatelessWidget {
   const listView({super.key});
@@ -16,8 +17,11 @@ class listView extends StatelessWidget {
           itemCount: list.length,
           itemBuilder: (BuildContext context, int index) {
             return ListTile(
-              leading: Icon(icons[index]),
-              title: Text(list[index]),
+              leading: Icon(icons[index], color: colors[index]),
+              title: Text(list[index], style: TextStyle(color: colors[index]),),
+              onTap: () => {
+                print('Tapped on ' + list[index])
+              },
             );
           },
         ));
